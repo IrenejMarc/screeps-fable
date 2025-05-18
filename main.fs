c7241d3e2bb2504a81c34ce1a825adb8d06dcc63
Irenej Marc <irenej.marc@gmail.com>
2025-05-18 15:38:01 +0000

    wow

--- main.fs
+++ main.fs
@@ -4,21 +4,18 @@ open ScreepsLib
 open Fable.Core
 open Fable.Core.JsInterop
 
-let random = new Random()
-
-let tap x fn =
-  fn x
-  x
+let randomInt min max =
+  min + JS.Math.random () * (max - min)
+  |> JS.Math.trunc
 
 let repeat (count: int) (fn : (int) -> unit) =
   {0 .. count - 1}
   |> Seq.iter (fun x -> fn x)
 
-let JSObject = JS.Constructors.Object
-let jsObjectKeys = JSObject.keys
+let jsObjectKeys = JS.Constructors.Object.keys
 let jsObjectSize = jsObjectKeys >> Seq.length
 
-let mySpawns = 
+let mySpawns () = 
   jsObjectKeys Game.spawns
   |> Seq.map (fun name -> Game.spawns?(name) :> Spawn)
 
@@ -27,21 +24,74 @@ let WANTED_CREEP_BODY = [| WORK; MOVE; CARRY |];
 
 let spawnMinersForSpawn (spawn: Spawn): unit =
   let minerCount = jsObjectSize Game.creeps
-
   let missingMinerCount = CREEPS_PER_ROOM - minerCount
 
   if missingMinerCount > 0 then
     repeat missingMinerCount $ fun _ ->
-      let creepName = $"C {random.Next(100, 900)}"
+      let creepName = $"C {randomInt 100 900}"
       let creepSpawnResult =
-        spawn.spawnCreep WANTED_CREEP_BODY creepName
+        spawn.spawnCreep WANTED_CREEP_BODY creepName (createObj [ "role", "builder" ])
 
       console.log $"Spawning missing creep '{creepName}' for {spawn.name}- result: {creepSpawnResult}"
 
 let spawnMiners () =
-  mySpawns |> Seq.iter spawnMinersForSpawn
+  mySpawns() |> Seq.iter spawnMinersForSpawn
+
+[<StringEnum>]
+type CreepRole =
+  | Miner
+  | Builder
+  | Upgrader
+
+[<AllowNullLiteral>]
+type MyCreepMemory =
+  abstract role: CreepRole option with get, set
+
+type MyCreep =
+  inherit Creep
+  abstract memory: MyCreepMemory with get, set
+
+[<StringEnum>]
+type CreepRunResult =
+  | Working
+  | Waiting
+  | TaskCompleted
+  | NotImplemented
+
+let myCreeps () = 
+  jsObjectKeys Game.creeps
+  |> Seq.map (fun name -> Game.creeps?(name) :> MyCreep)
+
+let runBuilder (creep: MyCreep): CreepRunResult =
+  console.log $"Creep {creep.name} is buldeirng"
+  Working
+
+let runMiner (creep: MyCreep) =
+  console.log $"Creep {creep.name} is working"
+  creep.room.find(FIND_SOURCES)
+  Working
+
+let runCreep (creep: MyCreep) =
+  match creep.memory.role with
+  | Some Builder  -> creep, runBuilder creep
+  | Some Miner    -> creep, runMiner creep
+  | Some Upgrader -> creep, NotImplemented
+  | None          -> creep, Waiting
+
+let handleRunResult ((creep, result): MyCreep * CreepRunResult) =
+  match result with
+  | Working -> ()
+  | Waiting -> console.log $"No task for {creep.name} :(" // TODO: assign new task!
+  | TaskCompleted -> () // TODO: ditto
+  | NotImplemented -> console.log "Task not implemented" // TODO: ditto
+
+let runCreeps () =
+  myCreeps()
+  |> Seq.map runCreep
+  |> Seq.iter handleRunResult
 
 let run () = do
-  spawnMiners ()
+  spawnMiners()
+  runCreeps()
 
 exportModule "loop" run
--- src/ScreepsLib.fs
+++ src/ScreepsLib.fs
@@ -10,19 +10,23 @@ let console: JS.Console = jsNative
 [<Global("Object")>]
 let JSObject: JS.Object = jsNative
 
-type [<AllowNullLiteral>] JSMapObject<'V> =
-  [<Emit("$0[$1]")>]
-  member self.get a: 'V = jsNative
+[<Global>]
+let FIND_SOURCES: float = jsNative
 
 type [<AllowNullLiteral>] Spawn =
   abstract name: string with get, set
-  abstract member spawnCreep: string array -> string -> JS.Object
+  abstract member spawnCreep: string array -> string -> obj -> float
+
+type [<AllowNullLiteral>] Room =
+  abstract member name: string
+  abstract member find: float -> options: obj -> ResizeArray<'a>
 
 type [<AllowNullLiteral>] Creep =
-  abstract name: string with get, set
+  abstract name:   string    with get, set
+  abstract memory: JS.Object with get, set 
+  abstract room: Room with get, set
 
 type [<AllowNullLiteral>] Memory =
-  [<Emit("$0[$1]{{=$2}}")>] abstract Item: name: string -> JS.Object with get, set
   abstract flags:  JS.Object with get, set
   abstract rooms:  JS.Object with get, set
   abstract spawns: JS.Object with get, set
@@ -32,24 +36,17 @@ type [<AllowNullLiteral>] Game =
   abstract spawns: JS.Object with get, set
   abstract creeps: JS.Object with get, set
 
-  abstract flags:             JS.Object   with get, set
-  abstract rooms:             JS.Object   with get, set
-  abstract structures:        JS.Object   with get, set
-  abstract constructionSites: JS.Object   with get, set
-  abstract time:              float with get, set
+  abstract flags:             JS.Object with get, set
+  abstract rooms:             JS.Object with get, set
+  abstract structures:        JS.Object with get, set
+  abstract constructionSites: JS.Object with get, set
+  abstract time:              float     with get, set
 
   abstract getObjectById: id: string -> 'T
 
 let [<Global>] WORK:  string = jsNative
 let [<Global>] MOVE:  string = jsNative
 let [<Global>] CARRY: string = jsNative
-
-type FindT = JS.Object
-
-type [<AllowNullLiteral>] Room =
-  abstract member name: string
-  abstract member find: FindT -> JS.Object
-
 
 let [<Global>] Memory: Memory = jsNative
 let [<Global>] Game: Game = jsNative
